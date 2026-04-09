a318e4b52f1047476cba03eb45bdebe68a6d3cc4
RamiroHH <ramiroh.dev@gmail.com>
2026-04-09 20:54:25 -0300

    Update IAuthService.java

--- ejercicio8/src/main/java/com/example/ejercicio8/auth/services/interfaces/IAuthService.java
+++ ejercicio8/src/main/java/com/example/ejercicio8/auth/services/interfaces/IAuthService.java
@@ -1,6 +1,5 @@
 package com.example.ejercicio8.auth.services.interfaces;
 
-
 import com.example.ejercicio8.auth.dtos.request.LoginRequestDto;
 import com.example.ejercicio8.auth.dtos.request.RegisterRequestDto;
 import com.example.ejercicio8.auth.dtos.response.AuthResponseDto;
@@ -8,5 +7,8 @@ import com.example.ejercicio8.auth.dtos.response.AuthResponseDto;
 public interface IAuthService {
 
     void register(RegisterRequestDto request);
+
     AuthResponseDto login(LoginRequestDto request);
+
+    AuthResponseDto refresh(String refreshToken);
 }
